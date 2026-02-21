b5644a0ee808027349905378e814403f2886c7c7
Ese_Victor <esevictor630@gmail.com>
2026-02-21 11:56:32 +0100

    Add LibraryItem and Book classes

--- library/models.py
+++ library/models.py
@@ -1,1 +1,15 @@
+class LibraryItem:
+    def __init__(self, title, item_id, is_checked_out=False):
+        self.title = title
+        self.item_id = item_id
+        self.is_checked_out = is_checked_out
 
+
+class Book(LibraryItem):
+    def __init__(self, title, item_id, author, is_checked_out=False):
+        super().__init__(title, item_id, is_checked_out)
+        self.author = author
+
+    def get_info(self):
+        status = "🔴 Checked Out" if self.is_checked_out else "🟢 Available"
+        return f"{self.item_id} | {self.title} | {self.author} | {status}"
